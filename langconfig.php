ec0a8b30d1951f0c41708b4c2d2c0b489c8e248c
Arndt von Lucadou <info@lucadou.net>
2015-12-21 16:14:24 +0100

    Add langconfig.php with some form translations

---
+++ langconfig.php
@@ -0,0 +1,13 @@
+<?php
+if ($GLOBALS['TL_LANGUAGE'] == 'de') {
+    $GLOBALS['TL_LANG']['custom']['message'] = 'Nachricht';
+    $GLOBALS['TL_LANG']['custom']['submitMessage'] = 'Nachricht abschicken';
+}
+elseif ($GLOBALS['TL_LANGUAGE'] == 'en') {
+    $GLOBALS['TL_LANG']['custom']['message'] = 'Message';
+    $GLOBALS['TL_LANG']['custom']['submitMessage'] = 'Submit message';
+}
+elseif ($GLOBALS['TL_LANGUAGE'] == 'fr') {
+    $GLOBALS['TL_LANG']['custom']['message'] = 'Message';
+    $GLOBALS['TL_LANG']['custom']['submitMessage'] = 'Envoyer le message';
+}
